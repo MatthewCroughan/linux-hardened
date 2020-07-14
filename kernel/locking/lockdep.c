83d1a7d9c41f0654ac517c3aeb2f415d92622325
Pavel Snajdr <snajpa@snajpa.net>
2020-07-14 21:45:25 +0200

    [vpsAdminOS] GPL-only symbols inhibit innovation
    Signed-off-by: Pavel Snajdr <snajpa@snajpa.net>

--- kernel/locking/lockdep.c
+++ kernel/locking/lockdep.c
@@ -1194,7 +1194,7 @@ out_unlock:
 restore_irqs:
 	raw_local_irq_restore(flags);
 }
-EXPORT_SYMBOL_GPL(lockdep_register_key);
+EXPORT_SYMBOL(lockdep_register_key);
 
 /* Check whether a key has been registered as a dynamic key. */
 static bool is_dynamic_key(const struct lock_class_key *key)
@@ -4754,10 +4754,10 @@ void lockdep_init_map_waits(struct lockdep_map *lock, const char *name,
 		raw_local_irq_restore(flags);
 	}
 }
-EXPORT_SYMBOL_GPL(lockdep_init_map_waits);
+EXPORT_SYMBOL(lockdep_init_map_waits);
 
 struct lock_class_key __lockdep_no_validate__;
-EXPORT_SYMBOL_GPL(__lockdep_no_validate__);
+EXPORT_SYMBOL(__lockdep_no_validate__);
 
 static void
 print_lock_nested_lock_not_held(struct task_struct *curr,
@@ -5447,7 +5447,7 @@ void lock_set_class(struct lockdep_map *lock, const char *name,
 	lockdep_recursion_finish();
 	raw_local_irq_restore(flags);
 }
-EXPORT_SYMBOL_GPL(lock_set_class);
+EXPORT_SYMBOL(lock_set_class);
 
 void lock_downgrade(struct lockdep_map *lock, unsigned long ip)
 {
@@ -5464,7 +5464,7 @@ void lock_downgrade(struct lockdep_map *lock, unsigned long ip)
 	lockdep_recursion_finish();
 	raw_local_irq_restore(flags);
 }
-EXPORT_SYMBOL_GPL(lock_downgrade);
+EXPORT_SYMBOL(lock_downgrade);
 
 /* NMI context !!! */
 static void verify_lock_unused(struct lockdep_map *lock, struct held_lock *hlock, int subclass)
@@ -5562,7 +5562,7 @@ void lock_acquire(struct lockdep_map *lock, unsigned int subclass,
 	lockdep_recursion_finish();
 	raw_local_irq_restore(flags);
 }
-EXPORT_SYMBOL_GPL(lock_acquire);
+EXPORT_SYMBOL(lock_acquire);
 
 void lock_release(struct lockdep_map *lock, unsigned long ip)
 {
@@ -5582,7 +5582,7 @@ void lock_release(struct lockdep_map *lock, unsigned long ip)
 	lockdep_recursion_finish();
 	raw_local_irq_restore(flags);
 }
-EXPORT_SYMBOL_GPL(lock_release);
+EXPORT_SYMBOL(lock_release);
 
 noinstr int lock_is_held_type(const struct lockdep_map *lock, int read)
 {
@@ -5602,7 +5602,7 @@ noinstr int lock_is_held_type(const struct lockdep_map *lock, int read)
 
 	return ret;
 }
-EXPORT_SYMBOL_GPL(lock_is_held_type);
+EXPORT_SYMBOL(lock_is_held_type);
 NOKPROBE_SYMBOL(lock_is_held_type);
 
 struct pin_cookie lock_pin_lock(struct lockdep_map *lock)
@@ -5623,7 +5623,7 @@ struct pin_cookie lock_pin_lock(struct lockdep_map *lock)
 
 	return cookie;
 }
-EXPORT_SYMBOL_GPL(lock_pin_lock);
+EXPORT_SYMBOL(lock_pin_lock);
 
 void lock_repin_lock(struct lockdep_map *lock, struct pin_cookie cookie)
 {
@@ -5640,7 +5640,7 @@ void lock_repin_lock(struct lockdep_map *lock, struct pin_cookie cookie)
 	lockdep_recursion_finish();
 	raw_local_irq_restore(flags);
 }
-EXPORT_SYMBOL_GPL(lock_repin_lock);
+EXPORT_SYMBOL(lock_repin_lock);
 
 void lock_unpin_lock(struct lockdep_map *lock, struct pin_cookie cookie)
 {
@@ -5657,7 +5657,7 @@ void lock_unpin_lock(struct lockdep_map *lock, struct pin_cookie cookie)
 	lockdep_recursion_finish();
 	raw_local_irq_restore(flags);
 }
-EXPORT_SYMBOL_GPL(lock_unpin_lock);
+EXPORT_SYMBOL(lock_unpin_lock);
 
 #ifdef CONFIG_LOCK_STAT
 static void print_lock_contention_bug(struct task_struct *curr,
@@ -5792,7 +5792,7 @@ void lock_contended(struct lockdep_map *lock, unsigned long ip)
 	lockdep_recursion_finish();
 	raw_local_irq_restore(flags);
 }
-EXPORT_SYMBOL_GPL(lock_contended);
+EXPORT_SYMBOL(lock_contended);
 
 void lock_acquired(struct lockdep_map *lock, unsigned long ip)
 {
@@ -5810,7 +5810,7 @@ void lock_acquired(struct lockdep_map *lock, unsigned long ip)
 	lockdep_recursion_finish();
 	raw_local_irq_restore(flags);
 }
-EXPORT_SYMBOL_GPL(lock_acquired);
+EXPORT_SYMBOL(lock_acquired);
 #endif
 
 /*
@@ -6243,7 +6243,7 @@ out_irq:
 	/* Wait until is_dynamic_key() has finished accessing k->hash_entry. */
 	synchronize_rcu();
 }
-EXPORT_SYMBOL_GPL(lockdep_unregister_key);
+EXPORT_SYMBOL(lockdep_unregister_key);
 
 void __init lockdep_init(void)
 {
@@ -6342,7 +6342,7 @@ void debug_check_no_locks_freed(const void *mem_from, unsigned long mem_len)
 	}
 	raw_local_irq_restore(flags);
 }
-EXPORT_SYMBOL_GPL(debug_check_no_locks_freed);
+EXPORT_SYMBOL(debug_check_no_locks_freed);
 
 static void print_held_locks_bug(void)
 {
@@ -6367,7 +6367,7 @@ void debug_check_no_locks_held(void)
 	if (unlikely(current->lockdep_depth > 0))
 		print_held_locks_bug();
 }
-EXPORT_SYMBOL_GPL(debug_check_no_locks_held);
+EXPORT_SYMBOL(debug_check_no_locks_held);
 
 #ifdef __KERNEL__
 void debug_show_all_locks(void)
@@ -6393,7 +6393,7 @@ void debug_show_all_locks(void)
 	pr_warn("\n");
 	pr_warn("=============================================\n\n");
 }
-EXPORT_SYMBOL_GPL(debug_show_all_locks);
+EXPORT_SYMBOL(debug_show_all_locks);
 #endif
 
 /*
@@ -6408,7 +6408,7 @@ void debug_show_held_locks(struct task_struct *task)
 	}
 	lockdep_print_held_locks(task);
 }
-EXPORT_SYMBOL_GPL(debug_show_held_locks);
+EXPORT_SYMBOL(debug_show_held_locks);
 
 asmlinkage __visible void lockdep_sys_exit(void)
 {
@@ -6477,4 +6477,4 @@ void lockdep_rcu_suspicious(const char *file, const int line, const char *s)
 	pr_warn("\nstack backtrace:\n");
 	dump_stack();
 }
-EXPORT_SYMBOL_GPL(lockdep_rcu_suspicious);
+EXPORT_SYMBOL(lockdep_rcu_suspicious);
